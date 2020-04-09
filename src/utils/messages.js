e9a82fb8487d21ed2d2cca0352d143d74d98b2f6
RiccardoPoli99 <riccardo.prestigiacomo@gmail.com>
2020-04-09 11:47:59 +0200

    First commit

---
+++ src/utils/messages.js
@@ -0,0 +1,21 @@
+//define a few functions that generate message object
+const generateMessage = (username, text) => {
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+const generateLocationMessage = (username, url) => {
+    return {
+        username,
+        url,
+        createdAt: new Date().getTime()
+    }
+}
+
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+}
